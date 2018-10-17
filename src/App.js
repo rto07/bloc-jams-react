cbb579f10f057bb0592e803cc7b247a9c255cd22
tar <rtorres845@gmail.com>
2018-10-17 15:48:53 -0500

    Landing Checkpoint Work

--- src/App.js
+++ src/App.js
@@ -22,6 +22,12 @@ class App extends Component {
           <h1>Bloc Jams</h1>
 
         </header>
+
+        <main>
+          <Route exact path="/" component={Landing}/>
+          <Route path="/library" component={Library} />
+          <Route path = "/album" component = {Album}/>
+        </main>
       </div>
     );
   }
