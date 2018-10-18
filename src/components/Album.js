7544595137e77d6c12464e30468f4928d5534827
tar <rtorres845@gmail.com>
2018-10-18 12:55:07 -0500

    Album Checkpoint Work

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,11 +1,41 @@
 import React, { Component } from 'react';
+import albumData from './../Data/albums';
 
 class Album extends Component {
+	constructor(props){
+		super(props);
+
+		const album = albumData.find(album => {
+			return album.slug === this.props.match.params.slug
+		});
+
+		this.state = {
+			album : album
+		};
+	}
+
 	render (){
 		return (
 			<section className = "album">
-				{this.props.match.params.slug} 
-				Album page will go here
+		        <section id = "album-info">
+		           <img id = "album-cover-art" src = {this.state.album.albumCover} alt = {this.state.album.title}/>
+		           <div className = "album-details">
+		             <h1 id = "album-title">{this.state.album.title}</h1>
+		             <h2 className = "artist">{this.state.album.artist}</h2>
+		             <div id = "release-info">{this.state.album.realeaseInfo}
+		             </div>
+		           </div>
+				</section>
+
+		        <tabel id = "song-list">
+		        	<colgroup>
+		        		<col id = "song-number-column" />
+		        		<col id = "song-title-column" />
+		        		<col id = "song-duration-column" />
+		        	</colgroup>
+		        	<tbody>
+		        	</tbody>
+		        </tabel>
 			</section> 
 		);
 	}
