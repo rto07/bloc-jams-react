31d379042368f4970e95c14cd87b5c19660bd064
tar <rtorres845@gmail.com>
2018-10-31 14:10:06 -0500

    Audio_Playback_Assignment

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,6 +1,31 @@
 import React, { Component } from 'react';
 import albumData from './../Data/albums';
 
+{/* Your event listeners should be placed alongside side this.handleSongClick():
+
+Move them from here:
+
+`<td className = "song-number"> <span className = "ion-play" this.onMouseEnter ><span className = "ion-pause" this.onMouseLeave>{index + 1}</span></span></td>`
+
+To here:
+
+ `<tr className = "song" key = {index} onClick = {() => this.handleSongClick(song)} >`
+
+You should not attempt to return a <span> alongside the event listeners.
+
+This is the approach you should take:
+
+1. add event listeners ONLY to the <tr> tag. 
+2. Pass parameter index into onMouseEnter()
+3. In onMouseEnter(index) assign index to `isMouseInside`
+4. Do not call `handleSongClick` inside onMouseEnter() and onMouseLeave()
+
+We can go through the logic to display icons in a <span> after. They will be part of a <td> tag 
+
+I would look up how to formulate event listeners. This is the proper way:
+
+onClick = {() => this.handleSongClick( song )}    
+onMouseEnter={() => this.onMouseEnter( index )}*/}
 
 class Album extends Component {
 	constructor(props){
@@ -46,84 +71,103 @@ class Album extends Component {
 			}
 		}
 
+		onMouseEnter( index) {
+			var index = "isMouseInside";
+			if ( this.setState.isHovered = true && this.state.currentSong ) {
+					onClick( this.play );
+			}
+		}
+
+		onMouseLeaving( index ) {
+			if ( this.setState.isHovered = false && this.state.currentSong ) {
+					onClick(this.pause );
+			}
+		}
 
 	render() {
 		
 		return (
+
 			
-			<section className = "album">
+			< section className = "album">
 		       
-		        <section id = "album-info">
+		        < section id = "album-info">
 		        
-		           <img id = "album-cover-art" src = {this.state.album.albumCover} alt = {this.state.album.title}/>
+		           < img id = "album-cover-art" src = { this.state.album.albumCover } alt = { this.state.album.title } />
 		           
-		           <div className = "album-details">
+		           < div className = "album-details" >
 		             
-		             <h1 id = "album-title" > {this.state.album.title}</h1>
+		             < h1 id = "album-title" > { this.state.album.title }< /h1 >
 		             
-		             <h2 className = "artist" > {this.state.album.artist}</h2>
+		             < h2 className = "artist" > { this.state.album.artist }< /h2 >
 		             
-		             <div id = "release-info" > {this.state.album.realeaseInfo}
-		             </div>
+		             < div id = "release-info" > { this.state.album.realeaseInfo }
+		             < /div >
 		           
-		           </div>
+		           < /div >
 				
-				</section>
+				< /section >
 
 		        <table id = "song-list">
-			    
-					<div className= "hover">
-			    
-			        	<colgroup>
+			    			    
+			        	< colgroup >
 
-			        		<col id = "song-number-column" />
-			        		<col id = "song-title-column" />
-			        		<col id = "song-duration-column" />
+			        		< col id = "song-number-column" />
+			        		< col id = "song-title-column" />
+			        		< col id = "song-duration-column" />
 
-			        	</colgroup>
+			        	< /colgroup >
 			        	
-			        	<tbody>
+			        	< tbody >
 
-			        		{this.state.album.songs.map( ( song, index ) =>
+			        		{this.state.album.songs.map( ( song, index ) => (
 				              
-			                  <tr className="song" key={index} onClick={() => this.handleSongClick(song)}
-			                    onMouseEnter={(isMouseInside) => this.setState({ isHovered: index + 1 })}
-			                    onMouseLeave={() => this.setState({ isHovered: false })}>
+			                < tr 
+			                  	className="song" 
+			                  	key={index} 
+			                  	onClick = {() => this.handleSongClick(song)}
 
-				              	<td className = "song-actions"> 
-				              		<button id="song-action-buttons">
-				              		{ ( this.state.currentSong.title === song.title ) ?
-			                        <span className = { this.state.isPlaying ? "ion-pause" : "ion-play" }>
-			                        </span>
+			                   	this.setState({ isHovered = true; })
 
-			                        :
+			                    document.getElementById(" song-list ").addEventListener("mouseEnter", onMouseEnter(isMouseInside));>
+			                   	
+			                   	this.setState({ isHovered: false; })}
 
-			                        ( this.state.isHovered === index + 1 ) ?
-			                        
-			                        <span className = "ion-play">
-			                        </span>
-			                        
-			                        :
-			                        
-			                        <span className = "song-number">{ index+1 }
-			                        </span>
-			                      }
+			                    document.getElementById(" song-list ").addEventListener("mouseLeave", onMouseLeave);>
+			                }
 
-			                      </button>
-
-				              	</td>
+				             < td 
+				              		className = "song-actions"> 
+				              		{''}
+				              		{ this.state.isPlaying ? (
+				              			< span >
+				              				{''}
+				              				{this.state.currentSong.title === song.title ? (
+						                <span className="ion-pause" />
+						              ) : (
+						                < span >
+						                	{index + 1}</span>
+						              		)}
+						            	< /span >
+						          		) : this.state.isHovered === index + 1 ? (
+						            < span className="ion-play" />
+						          ) : (
+						            < span className="song-number">{index + 1}< /span >
+						          )}
+						        < /td >
 				                
-				                <td className = "song-title">{ song.title }
-				                </td>
+				                < td 
+				                	className = "song-title">{ song.title }
+				                < /td >
 				                
-				                <td className = "song-duration">{ song.duration }
+				                <td 
+				                	className = "song-duration">{ song.duration }
 				                </td>
 				              
 				              </tr>
-				            )}
+				            ))}
 
 	          			</tbody>
-	          		</div>
 			 	</table>		
 			</section>
 		)
