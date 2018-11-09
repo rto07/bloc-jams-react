3cfbd802ea1c5e58a0a6cc845bfa386cfe89d9f0
tar <rtorres845@gmail.com>
2018-11-09 12:23:06 -0600

    bjr-playBar_Buttons-checkpointWork

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import albumData from './../Data/albums';
+import PlayerBar from './PlayerBar';
 
 
 class Album extends Component {
@@ -64,29 +65,46 @@ class Album extends Component {
             return (<span className="icon ion-md-play"></span>);
         } else {
             return (index + 1);
+
         }
     }
+
+	handlePrevClick() {
+		const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong -1);
+		const newIndex = Math.max(0, currentIndex - 1);
+		const newSong = this.state.album.songs[newIndex];
+		this.play();
+	}
 
 
     render() {
         return (
+
             <section className="album">
+
                 <section id="album-info">
+
                     <img id="album-cover-art" src={this.state.album.albumCover} alt={this.state.album.title} />
+
                     <div className="album-details">
                         <h1 id="album-title">{this.state.album.title}</h1>
                         <h2 className="artist">{this.state.album.artist}</h2>
                         <div id="release-info">{this.state.album.releaseInfo}</div>
                     </div>
+
                 </section>
+
                 <table id="song-list">
+                    
                     <colgroup>
                         <col id="song-number-column" />
                         <col id="song-title-column" />
                         <col id="song-duration-column" />
                     </colgroup>
+                   
                     <tbody>
                         {this.state.album.songs.map((song, index) =>
+                            
                             <tr className="song" key={index}
                                 onClick={() => this.handleSongClick(song)}
                                 onMouseEnter={() => this.onMouseEnter(index)}
@@ -98,7 +116,22 @@ class Album extends Component {
                         )
                         }
                     </tbody>
+               
                 </table>
+                
+
+                <PlayerBar 
+                	
+                	isPlaying = {this.state.isPlaying}
+
+                	currentSong = {this.state.currentSong} 
+                	
+                	handleSongClick = {() => this.handleSongClick(this.state.currentSong)}
+
+                	handlePrevClick = {() => this.handlePrevClick()}
+                />
+
+
             </section>
         );
     };
